8c24bb2adab0f152419711dc19bd03ae4f218518
tiranflorin <tiran.florin@gmail.com>
2018-02-04 01:23:50 +0200

    Updated the action buttons handlers. Tweaked the UI.

---
+++ README.md
@@ -0,0 +1,7 @@
+Security Policies Client
+========================
+
+Just change the url value from "js/main.js" file in order to connect the client to the REST api.
+
+
+Enjoy!
--- js/main.js
+++ js/main.js
@@ -1,124 +1,131 @@
-$( document ).ready(function() {
-
+$(document).ready(function () {
     var url = 'http://localhost/html/projects/security-policies-symf3.4/web/app_dev.php/policy';
 
     getPageData();
 
-    /* Get Page Data*/
     function getPageData() {
-        debugger;
         $.ajax({
             dataType: 'json',
-            url: url,
-        }).done(function(data){
-            debugger;
+            url: url
+        }).done(function (data) {
             manageRow(data);
-        }).fail(function( jqXHR, textStatus, errorThrown ) {
+        }).fail(function (jqXHR, textStatus, errorThrown) {
             debugger;
         })
     };
-    
 
     /* Add new Item table row */
     function manageRow(data) {
-        var	rows = '';
-        $.each( data, function( key, value ) {
+        var rows = '';
+        $.each(data, function (key, value) {
             rows = rows + '<tr>';
-            rows = rows + '<td>'+value.name+'</td>';
-            rows = rows + '<td>'+value.implicitAction+'</td>';
-            rows = rows + '<td data-id="'+value.id+'">';
-            rows = rows + '<button data-toggle="modal" data-target="#edit-item" class="btn btn-primary edit-item">Edit</button> ';
-            rows = rows + '<button class="btn btn-danger remove-item">Delete</button>';
+            rows = rows + '<td>' + value.name + '';
+            // Just consider the first row the default policy. TODO: Enforce this on the back-end.
+            if (key === 0) {
+                rows = rows + ' (Default policy)';
+            }
+            rows = rows + '</td><td>' + value.implicitAction + '</td>';
+            rows = rows + '<td data-id="' + value.id + '">';
+            rows = rows + '<button data-toggle="modal" data-target="#clone-item" class="btn btn-success clone-item">Clone</button> ';
+            if (key != 0) {
+                rows = rows + '<button data-toggle="modal" data-target="#edit-item" class="btn btn-info edit-item">Edit</button> ';
+                rows = rows + '<button class="btn btn-danger remove-item">Delete</button>';
+            }
             rows = rows + '</td>';
             rows = rows + '</tr>';
         });
-
-        debugger;
-
         $("tbody").html(rows);
     }
 
-    /* Create new Item */
-    $(".crud-submit").click(function(e){
-        e.preventDefault();
-        var form_action = $("#create-item").find("form").attr("action");
-        var name = $("#create-item").find("input[name='name']").val();
-        var implicit_action = $("#create-item").find("textarea[name='implicit_action']").val();
-
-        if(name != '' && implicit_action != ''){
-            $.ajax({
-                dataType: 'json',
-                type:'POST',
-                url: url + form_action,
-                data:{name:name, implicit_action:implicit_action}
-            }).done(function(data){
-                $("#create-item").find("input[name='name']").val('');
-                $("#create-item").find("textarea[name='implicit_action']").val('');
-                getPageData();
-                $(".modal").modal('hide');
-                toastr.success('Item Created Successfully.', 'Success Alert', {timeOut: 5000});
-            });
-        }else{
-            alert('You are missing name or implicit_action.')
-        }
-
-    });
-
     /* Remove Item */
-    $("body").on("click",".remove-item",function(){
+    $("body").on("click", ".remove-item", function () {
         var id = $(this).parent("td").data('id');
         var c_obj = $(this).parents("tr");
 
         $.ajax({
             dataType: 'json',
-            type:'POST',
-            url: url + 'api/delete.php',
-            data:{id:id}
-        }).done(function(data){
+            type: 'DELETE',
+            url: url + '/' + id
+        }).done(function (data) {
             c_obj.remove();
-            toastr.success('Item Deleted Successfully.', 'Success Alert', {timeOut: 5000});
+            toastr.success('Item Deleted Successfully.', 'Success!', {timeOut: 5000});
             getPageData();
         });
-
     });
 
-    /* Edit Item */
-    $("body").on("click",".edit-item",function(){
+    /* Update Item */
+    $(".crud-submit-edit").click(function (e) {
+        e.preventDefault();
+        var name = $("#edit-item").find("input[name='name']").val();
+        var implicit_action = $("#edit-item").find("textarea[name='implicit_action']").val();
+        var id = $("#edit-item").find(".edit-id").val();
+        var data = {name: name, implicit_action: implicit_action};
 
+        if (name != '' && implicit_action != '') {
+            $.ajax({
+                dataType: 'json',
+                type: 'PUT',
+                url: url + '/' + id,
+                beforeSend: function (xhr) {
+                    xhr.overrideMimeType("application/json; charset=UTF-8");
+                },
+                data: JSON.stringify(data)
+            }).done(function (data) {
+                getPageData();
+                $(".modal").modal('hide');
+                toastr.success('Item Updated Successfully.', 'Success!', {timeOut: 5000});
+            });
+        } else {
+            alert('You are missing name or implicit_action.')
+        }
+    });
+
+    /* Clone new Item */
+    $(".crud-submit-clone").click(function (e) {
+        e.preventDefault();
+        var name = $("#clone-item").find("input[name='name']").val();
+        var implicit_action = $("#clone-item").find("textarea[name='implicit_action']").val();
+        var cloneId = $("#clone-item").find(".clone-id").val();
+        var data = {cloneId: cloneId, name: name, implicit_action: implicit_action};
+
+        if (name != '' && implicit_action != '') {
+            $.ajax({
+                dataType: 'json',
+                type: 'POST',
+                url: url,
+                beforeSend: function (xhr) {
+                    xhr.overrideMimeType("application/json; charset=UTF-8");
+                },
+                data: JSON.stringify(data)
+            }).done(function (data) {
+                getPageData();
+                $(".modal").modal('hide');
+                toastr.success('Item Cloned Successfully.', 'Success!', {timeOut: 5000});
+            });
+        } else {
+            alert('You are missing name or implicit_action.')
+        }
+    });
+
+    /* Populate Edit Form */
+    $("body").on("click", ".edit-item", function () {
         var id = $(this).parent("td").data('id');
         var name = $(this).parent("td").prev("td").prev("td").text();
         var implicit_action = $(this).parent("td").prev("td").text();
-
         $("#edit-item").find("input[name='name']").val(name);
         $("#edit-item").find("textarea[name='implicit_action']").val(implicit_action);
         $("#edit-item").find(".edit-id").val(id);
 
     });
 
-    /* Updated new Item */
-    $(".crud-submit-edit").click(function(e){
-
-        e.preventDefault();
-        var form_action = $("#edit-item").find("form").attr("action");
-        var name = $("#edit-item").find("input[name='name']").val();
-
-        var implicit_action = $("#edit-item").find("textarea[name='implicit_action']").val();
-        var id = $("#edit-item").find(".edit-id").val();
-
-        if(name != '' && implicit_action != ''){
-            $.ajax({
-                dataType: 'json',
-                type:'POST',
-                url: url + form_action,
-                data:{name:name, implicit_action:implicit_action,id:id}
-            }).done(function(data){
-                getPageData();
-                $(".modal").modal('hide');
-                toastr.success('Item Updated Successfully.', 'Success Alert', {timeOut: 5000});
-            });
-        }else{
-            alert('You are missing name or implicit_action.')
-        }
+    /* Populate Clone Form */
+    $("body").on("click", ".clone-item", function () {
+        var id = $(this).parent("td").data('id');
+        var name = $(this).parent("td").prev("td").prev("td").text();
+        var implicit_action = $(this).parent("td").prev("td").text();
+        $("#clone-item").find("input[name='name']").val(name);
+        $("#clone-item").find("textarea[name='implicit_action']").val(implicit_action);
+        $("#clone-item").find(".clone-id").val(id);
 
     });
 });
